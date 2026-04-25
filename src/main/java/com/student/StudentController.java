9d1e1e9e78baab4828c3a8cc16dfc525a359c73c
SaiDurgaPrasadVasamsetti <139942863+SaiDurgaPrasadVasamsetti@users.noreply.github.com>
2026-04-25 18:13:00 +0530

    Update StudentController.java

--- src/main/java/com/student/StudentController.java
+++ src/main/java/com/student/StudentController.java
@@ -1,1 +1,26 @@
 
+package com.student;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class StudentController {
+
+    private final StudentRepository repo;
+
+    public StudentController(StudentRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAll() {
+        return repo.findAll();
+    }
+
+    @PostMapping("/students")
+    public Student add(@RequestBody Student s) {
+        return repo.save(s);
+    }
+}
